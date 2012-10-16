d2a1cb60a2258c380cb18eb3a69730dfeec8c9e0
Constance Yang <yangyang@Constance.local>
2012-10-16 14:35:56 +0100

    Initial Commit

---
+++ Cocos2DSimpleGame/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Cocos2DSimpleGame
+//
+//  Created by Constance Yang on 12-10-16.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
